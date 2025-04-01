0e8fb43d5dd0e265c9ac2fdbb2d82dc43a53aed1
Suwonative <suwonative12@gmail.com>
2025-04-01 17:44:34 +0900

    Bit_Project1 model2

--- Bit_Project1/src/main/java/handler/CommandHandler.java
+++
@@ -1,9 +0,0 @@
-package handler;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
-public interface CommandHandler {
-	public String process(HttpServletRequest request, HttpServletResponse response)
-		throws Exception;
-}
--- Bit_Project1/src/main/webapp/error/nullPointer.jsp
+++
@@ -1,9 +0,0 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-    
-<h2> NullPointerException 예외 페이지 </h2>
-<%
-	response.setStatus(HttpServletResponse.SC_OK);
-%>
-NullPointerException이 발생했습니다. <br>
-망했습니다. <br>
